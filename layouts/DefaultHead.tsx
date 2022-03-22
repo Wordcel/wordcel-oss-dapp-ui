d44eb8b3649f7c6eb5eeee3f55f14e6a824e6d8b
Kunal Bagaria <kb24x7@gmail.com>
2022-03-23 00:26:29 +0530

    fix: fonts

--- layouts/DefaultHead.tsx
+++ layouts/DefaultHead.tsx
@@ -8,5 +8,5 @@ export const DefaultHead = () => {
       <link rel="icon" href="/favicon.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </Head>
-  )
+  );
 }
