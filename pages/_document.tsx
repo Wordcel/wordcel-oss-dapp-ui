47d6b33a8db23d58fcdac6bde055a971462786f0
Kunal Bagaria <kb24x7@gmail.com>
2022-04-12 23:43:46 +0530

    test: font fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=optional" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Spectral:wght@400;500;600;700;800&display=optional" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Spectral:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
