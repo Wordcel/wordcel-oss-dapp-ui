39f4efbecd649e86cf3d742ca85519dfbee54804
Kunal Bagaria <kb24x7@gmail.com>
2022-04-27 00:04:23 +0530

    fixes: design

--- layouts/ArticlePreview.tsx
+++ layouts/ArticlePreview.tsx
@@ -14,14 +14,16 @@ export const ArticlePreview = ({
   return (
     <div className={styles.articleContainer}>
       <p className="heading md nm">{article.title}</p>
-      <div className={styles.articleDescription}>
-        <p className="normal-text cs">
-          {shortenSentence(article.description)}
-        </p>
-        <p
-          onClick={() => router.push(`/${user?.username}/${article.slug}`)}
-          className="blue-text pointer">
-          READ MORE
+      <div>
+        <p>
+          <span className="normal-text cs">
+            {shortenSentence(article.description)}
+          </span>
+          <span
+            onClick={() => router.push(`/${user?.username}/${article.slug}`)}
+            className="blue-text pointer ml-1 op-1">
+            READ MORE
+          </span>
         </p>
       </div>
       <img className={styles.articleImage} src={article.image_url} />
