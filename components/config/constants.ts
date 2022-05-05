2ecc53f419409173ab21173efb0f01797b7e2a75
Kunal Bagaria <kb24x7@gmail.com>
2022-05-05 19:00:06 +0530

    change: endpoint | temp

--- components/config/constants.ts
+++ components/config/constants.ts
@@ -1,5 +1,6 @@
 export const MESSAGE_TO_SIGN = 'WORDCEL';
-export const ENDPOINT = 'https://psytrbhymqlkfrhudd.dev.genesysgo.net:8899/';
+// export const ENDPOINT = 'https://psytrbhymqlkfrhudd.dev.genesysgo.net:8899/';
+export const ENDPOINT = 'https://api.devnet.solana.com/';
 export const CLUSTER = 'devnet';
 export const BUNDLR_DEVNET_ENDPOINT = 'https://devnet.bundlr.network';
 export const BUNDLR_MAINNET_ENDPOINT = 'https://node1.bundlr.network';
