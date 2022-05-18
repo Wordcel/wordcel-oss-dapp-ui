e5bb77f3294fc2869e16a429b331855dba97d95d
Kunal Bagaria <kb24x7@gmail.com>
2022-05-18 12:38:11 +0530

    fixes

--- layouts/NewArticle.tsx
+++ layouts/NewArticle.tsx
@@ -5,21 +5,16 @@ import { publishPost } from '@/components/contractInteraction';
 import { EditorCore } from "@react-editor-js/core";
 import { useAnchorWallet, useWallet } from '@solana/wallet-adapter-react';
 import { useRouter } from 'next/router';
-import { useEffect, useCallback, useRef, useState } from 'react';
+import { useEffect, useCallback, useRef } from 'react';
 import { DefaultHead } from './DefaultHead';
 import { StaticNavbar } from './Navbar';
 import { getUserSignature } from '@/components/signMessage';
-import { uploadNFTStorage } from '@/components/upload';
-import { createNewCache } from '@/components/cache';
 import { Footer } from './Footer';
 
 
 export const NewArticle = () => {
   const router = useRouter();
   const { publicKey, signMessage } = useWallet();
-  const [articleId, setArticleId] = useState('');
-  const [sigError, setSigError] = useState('');
-  const [signature, setSignature] = useState<Uint8Array>();
   const anchorWallet = useAnchorWallet();
   const wallet = useWallet();
 
@@ -31,42 +26,6 @@ export const NewArticle = () => {
   const handleInitialize = useCallback((instance) => {
     editorInstance.current = instance
   }, []);
-
-  useEffect(() => {
-    (async function () {
-      if (publicKey && signMessage) {
-        const userSignature = await getUserSignature(signMessage);
-        if (!userSignature) {
-          toast('Please sign the message on your wallet so that we can save your progress');
-          setSigError(`Error: ${Math.random()}`)
-          return;
-        };
-        setSignature(userSignature);
-      }
-    })();
-  }, [sigError]);
-
-  useEffect(() => {
-    setInterval(async () => {
-      if (!editorInstance.current || !publicKey || !signature) return;
-      toast('Saving...')
-      const data = await editorInstance.current.save();
-      const payload = {
-        content: { blocks: data.blocks },
-        type: 'blocks'
-      };
-      const cache_link = await uploadNFTStorage(payload);
-      if (!cache_link) return;
-      const update_cache = await createNewCache({
-        id: articleId,
-        cache_link: cache_link,
-        signature: signature,
-        public_key: publicKey.toBase58()
-      });
-      console.log(update_cache);
-      setArticleId(update_cache.article.id.toString());
-    }, 30000)
-  }, [signature]);
 
   const handlePublish = async () => {
     if (!anchorWallet) return;
