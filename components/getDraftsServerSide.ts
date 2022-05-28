e1237c125ca57d8ed5b9a6da613789b6aaebd887
Kunal Bagaria <kb24x7@gmail.com>
2022-05-28 21:56:11 +0530

    add: descending order for drafts

--- components/getDraftsServerSide.ts
+++ components/getDraftsServerSide.ts
@@ -17,6 +17,9 @@ export const getDraftsServerSide = async (context: any) => {
   const drafts = await prisma.draft.findMany({
     where: {
       user_id: user.id
+    },
+    orderBy: {
+      created_at: 'desc'
     }
   });
   return {
