c9096849f97aaf05d317d5148b74377ca2bf742a
Kunal Bagaria <kb24x7@gmail.com>
2022-06-14 17:35:53 +0530

    add: redirects

--- next.config.js
+++ next.config.js
@@ -1,12 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/shek/:slug',
+        destination: '/shek.sol/:slug',
+        permanent: true,
+      },
+      {
+        source: '/kunal/:slug',
+        destination: '/kunal.sol/:slug',
+        permanent: true,
+      },
+      {
+        source: '/paarug/:slug',
+        destination: '/paarug.sol/:slug',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 const { withSentryConfig } = require('@sentry/nextjs');
 
 const sentryWebpackPluginOptions = {
-  silent: true, // Suppresses all logs
+  silent: true,
+  dryRun: process.env.VERCEL_ENV !== "production"
 };
 
 module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
