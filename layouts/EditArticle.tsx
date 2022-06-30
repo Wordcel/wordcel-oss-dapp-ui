9c7d40ae3ffe085dcb20293285d3ba7c35cd947e
Kunal Bagaria <kb24x7@gmail.com>
2022-06-30 22:13:40 +0530

    remove: auto save from published articles edit page

--- layouts/EditArticle.tsx
+++ layouts/EditArticle.tsx
@@ -9,9 +9,7 @@ import { useRouter } from 'next/router';
 import { useEffect, useCallback, useRef, useState } from 'react';
 import { DefaultHead } from './DefaultHead';
 import { StaticNavbar } from './Navbar';
-import { saveToast } from '@/components/saveToast';
 import { getUserSignature } from '@/lib/signMessage';
-import { deleteDraft, updateDraft } from '@/components/networkRequests';
 import { Footer } from './Footer';
 
 export const EditArticle = (props: GetArticleServerSide) => {
@@ -19,11 +17,8 @@ export const EditArticle = (props: GetArticleServerSide) => {
   const router = useRouter();
   const anchorWallet = useAnchorWallet();
   const [blocks] = useState<any>(JSON.parse(props.blocks || ''));
-  const [sigError, setSigError] = useState('');
-  const [signature, setSignature] = useState<Uint8Array>();
   const { publicKey, signMessage } = useWallet();
 
-  let [draft_id] = useState('');
   let [publishClicked] = useState(false);
   const Editor: any = dynamic(() => import('@/layouts/Editor'), {
     ssr: false
@@ -33,55 +28,6 @@ export const EditArticle = (props: GetArticleServerSide) => {
   const handleInitialize = useCallback((instance) => {
     editorInstance.current = instance
   }, []);
-
-  useEffect(() => {
-    (async function () {
-      if (publicKey && signMessage) {
-        const userSignature = await getUserSignature(signMessage, publicKey.toBase58());
-        if (!userSignature) {
-          toast('Please sign the message on your wallet so that we can save your progress');
-          setSigError(`Error: ${Math.random()}`)
-          return;
-        };
-        setSignature(userSignature);
-      }
-    })();
-  }, [sigError]);
-
-  useEffect(() => {
-    const eventListener = (e: KeyboardEvent) => {
-      if (e.keyCode === 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-        e.preventDefault();
-        saveToast();
-      }
-    }
-    if (typeof document !== 'undefined') {
-      document.addEventListener("keydown", eventListener);
-    }
-    return () => {
-      if (typeof document !== 'undefined') {
-        document.removeEventListener("keydown", eventListener);
-      }
-    }
-  }, [])
-
-  useEffect(() => {
-    const interval = setInterval(async () => {
-      if (!editorInstance.current?.save || !publicKey || !signature || publishClicked) return;
-      const data = await editorInstance.current.save();
-      const response = await updateDraft({
-        id: draft_id,
-        blocks: data.blocks,
-        signature: signature,
-        public_key: publicKey.toBase58()
-      });
-      console.log(response);
-      draft_id = response.draft.id;
-    }, 15000)
-    return () => {
-      clearInterval(interval);
-    }
-  }, [signature]);
 
   const handlePublish = async () => {
     if (!anchorWallet || !props.article || publishClicked) return;
@@ -109,11 +55,6 @@ export const EditArticle = (props: GetArticleServerSide) => {
       toast.error('Failed to publish article');
       return;
     };
-    deleteDraft({
-      id: draft_id?.toString(),
-      signature: signature,
-      public_key: wallet.publicKey?.toBase58()
-    });
     toast('Redirecting...');
     router.push(`/${response.username}/${response.article.slug}`);
   }
