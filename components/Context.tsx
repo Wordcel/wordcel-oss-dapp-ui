f7f8e894f0c8ac4f9124585bd3c00fdfa9940031
Kunal Bagaria <kb24x7@gmail.com>
2022-07-30 01:12:29 +0530

    add: editor context provider

--- components/Context.tsx
+++ components/Context.tsx
@@ -2,19 +2,28 @@ import {
   useEffect,
   useContext,
   createContext,
-  useState
+  useState,
+  useRef
 } from 'react';
+import { EditorCore } from "@react-editor-js/core";
 import { useWallet } from '@solana/wallet-adapter-react';
 import { getUser } from '@/lib/networkRequests';
 import { User } from '@prisma/client';
 
-interface Data {
+interface UserData {
   user: User | null;
 }
 
-const Context = createContext<Data | null>(null);
+interface EditorData {
+  instance: EditorCore | null;
+  initialize: (instance: any) => void;
+  remove: () => void;
+}
 
-const Provider = ({ children }: any) => {
+const UserContext = createContext<UserData | null>(null);
+const EditorContext = createContext<EditorData | null>(null);
+
+const UserProvider = ({ children }: any) => {
   const wallet = useWallet();
   const [user, setUser] = useState<null | User>(null);
 
@@ -29,12 +38,39 @@ const Provider = ({ children }: any) => {
   }, [wallet.publicKey]);
 
   return (
-    <Context.Provider value={user as any}>
+    <UserContext.Provider value={user as any}>
       {children}
-    </Context.Provider>
+    </UserContext.Provider>
   );
 }
 
-export default Provider;
+const EditorProvider = ({ children }: any) => {
+  const [instance, setInstance] = useState<EditorCore | null>(null);
 
-export const useUser = () => useContext(Context);
+  const initialize = (instance_: any) => {
+    setInstance(instance_);
+  };
+  const remove = () => {
+    setInstance(null);
+  };
+
+  return (
+    <EditorContext.Provider value={{
+      instance,
+      initialize,
+      remove
+    }}>
+      {children}
+    </EditorContext.Provider>
+  );
+}
+
+const useEditor = () => useContext(EditorContext);
+const useUser = () => useContext(UserContext);
+
+export {
+  useUser,
+  UserProvider,
+  useEditor,
+  EditorProvider
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import Router from 'next/router';
 import NProgress from 'nprogress';
-import UserProvider from '@/components/Context';
 
 import 'nprogress/nprogress.css';
 import 'inter-ui/inter.css';
 import '@fontsource/spectral';
 import '@/styles/globals/index.scss';
 
+import { UserProvider, EditorProvider } from '@/components/Context';
 import { NextUIProvider } from '@nextui-org/react';
 import { Toaster } from 'react-hot-toast';
 import { Wallet } from '@/layouts/Wallet';
@@ -25,7 +25,9 @@ function Worcel({ Component, pageProps }: any) {
           <div style={{ fontSize: '170%' }}>
             <Toaster />
           </div>
-          <Component {...pageProps} />
+          <EditorProvider>
+            <Component {...pageProps} />
+          </EditorProvider>
         </UserProvider>
       </Wallet>
     </NextUIProvider>
