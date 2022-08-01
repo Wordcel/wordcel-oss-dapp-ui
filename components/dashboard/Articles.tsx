b4b86f7503ea8d44382402d0e34bdf6009099ac1
Kunal Bagaria <kb24x7@gmail.com>
2022-08-02 01:04:04 +0530

    add: loading animation

--- components/dashboard/Articles.tsx
+++ components/dashboard/Articles.tsx
@@ -1,13 +1,18 @@
 // Styles Imports
 import styles from '@/styles/Dashboard.module.scss';
+import { Article, Draft } from '@/types/props';
 
 // Component Imports
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Loading } from '../animations/Loading';
 import { useUser } from '../Context';
 
 export const DashboardArticles = () => {
   const data = useUser();
   const [tab, setTab] = useState("published");
+  const [loading, setLoading] = useState(true);
+  const [drafts, setDrafts] = useState<Draft[]>([]);
+  const [articles, setArticles] = useState<Article[]>([]);
 
   const tabIsActive = (_tab: string) =>
     tab === _tab ? 'weight-600 gray-600' : 'weight-400 gray-400';
@@ -15,7 +20,20 @@ export const DashboardArticles = () => {
   const sliderLocation = () =>
     tab === 'drafts' ? styles.draftsSlider : '';
 
-  const tabOnClick = (_tab: string) => setTab(_tab);
+  const tabOnClick = (_tab: string) => {
+    setLoading(true);
+    setTab(_tab);
+  };
+
+  useEffect(() => {
+    // Add articles fetch
+
+  }, []);
+
+  useEffect(() => {
+    // Add drafts fetch
+
+  }, []);
 
   return (
     <div className={styles.articlesContainer}>
@@ -24,6 +42,11 @@ export const DashboardArticles = () => {
         <p onClick={() => tabOnClick('drafts')} className={`text pointer nm size-16 ${tabIsActive('drafts')} ml-2`}>Drafts</p>
         <div className={`${styles.slider} ${sliderLocation()}`} />
       </div>
+      {loading && (
+        <div className={styles.loading}>
+          <Loading width={200} height={200} />
+        </div>
+      )}
     </div>
   )
 }
