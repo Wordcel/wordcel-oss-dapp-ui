396b3d86463acca9bd75a9e724726ca6e7fce6e8
Kunal Bagaria <kb24x7@gmail.com>
2022-08-17 16:50:51 +0530

    fix: google analytics

--- components/DefaultHead.tsx
+++ components/DefaultHead.tsx
@@ -13,32 +13,34 @@ interface SEO {
 
 export const DefaultHead = (config: SEO) => {
   return (
-    <Head>
-      <title>{config.title || 'Wordcel - Read, Write, Own'}</title>
-      <meta name='description' content={config.description || 'Wordcel - Read, Write, Own'} />
-      <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <meta name="theme-color" content="white" />
-      <link rel='icon' href='/favicon.png' />
+    <>
+      <Head>
+        <title>{config.title || 'Wordcel - Read, Write, Own'}</title>
+        <meta name='description' content={config.description || 'Wordcel - Read, Write, Own'} />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name="theme-color" content="white" />
+        <link rel='icon' href='/favicon.png' />
 
-      {config.author && (
-        <meta name='author' content={config.author} />
-      )}
+        {config.author && (
+          <meta name='author' content={config.author} />
+        )}
 
-      {config.blog_name && (
-        <meta name='og:site_name' content={config.blog_name} />
-      )}
+        {config.blog_name && (
+          <meta name='og:site_name' content={config.blog_name} />
+        )}
 
-      <meta property='og:type' content={config.author ? 'article' : 'website'} />
-      <meta property='og:url' content={config.url || 'https://wordcelclub.com'} />
-      <meta property='og:title' content={config.title || 'Wordcel - Read, Write, Own'} />
-      <meta property='og:description' content={config.description || 'Wordcel - Read, Write, Own'} />
-      <meta property='og:image' content={config.image || DEFAULT_OG_IMAGE} />
+        <meta property='og:type' content={config.author ? 'article' : 'website'} />
+        <meta property='og:url' content={config.url || 'https://wordcelclub.com'} />
+        <meta property='og:title' content={config.title || 'Wordcel - Read, Write, Own'} />
+        <meta property='og:description' content={config.description || 'Wordcel - Read, Write, Own'} />
+        <meta property='og:image' content={config.image || DEFAULT_OG_IMAGE} />
 
-      <meta property='twitter:card' content='summary_large_image' />
-      <meta property='twitter:url' content={config.url || 'https://wordcelclub.com'} />
-      <meta property='twitter:title' content={config.title || 'Wordcel - Read, Write, Own'} />
-      <meta property='twitter:description' content={config.description || 'Wordcel - Read, Write, Own'} />
-      <meta property='twitter:image' content={config.image || DEFAULT_OG_IMAGE} />
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content={config.url || 'https://wordcelclub.com'} />
+        <meta property='twitter:title' content={config.title || 'Wordcel - Read, Write, Own'} />
+        <meta property='twitter:description' content={config.description || 'Wordcel - Read, Write, Own'} />
+        <meta property='twitter:image' content={config.image || DEFAULT_OG_IMAGE} />
+      </Head>
 
       {/* Google Analytics -- Start */}
       <Script
@@ -51,12 +53,11 @@ export const DefaultHead = (config: SEO) => {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-
           gtag('config', 'G-WZLDV8RBNC');
         `}
       </Script>
       {/* Google Analytics -- End */}
 
-    </Head>
+    </>
   );
 };
