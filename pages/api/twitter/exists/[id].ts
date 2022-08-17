c4752091d3b6dc7cfc1a8952db54e3b88d1b2999
Kunal Bagaria <kb24x7@gmail.com>
2022-08-17 22:40:21 +0530

    add: validation api for twitter table

---
+++ pages/api/twitter/exists/[id].ts
@@ -0,0 +1,30 @@
+import prisma from '@/lib/prisma';
+import type {
+  NextApiRequest,
+  NextApiResponse,
+} from 'next';
+import { withSentry } from '@sentry/nextjs';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { id } = req.query;
+  const exists = await prisma.verifiedTwitter.findFirst({
+    where: {
+      twitter_user_id: id as string
+    }
+  });
+  if (!exists) {
+    res.status(404).json({
+      error: 'Twitter user does not exist'
+    });
+    return;
+  };
+  res.status(200).json({
+    message: 'User has a verified Wordcel account',
+    username: exists.username
+  });
+};
+
+export default withSentry(handler);
