0cc6fc9f21926991cfdaecaaa858a26070720757
Kunal Bagaria <kb24x7@gmail.com>
2022-09-02 00:35:42 +0530

    complete: view count

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,3 +3,11 @@ export const timeout = (ms: number) => new Promise(resolve => setTimeout(resolve
 export const getFirstName = (fullName?: string) => {
   return fullName?.split(' ')[0];
 }
+
+export const numformat = (n: number) => {
+  if (n < 1e3) return n;
+  if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + "K";
+  if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + "M";
+  if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + "B";
+  if (n >= 1e12) return +(n / 1e12).toFixed(1) + "T";
+};
