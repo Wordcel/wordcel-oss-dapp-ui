7b5c778cd9e6b639b2d60064a789e332068d4184
vjgpt <vjgupta57@gmail.com>
2022-09-23 00:05:53 +0530

    updating get article api and creating api to get total connections of a user

---
+++ pages/api/connections/total/[public_key].ts
@@ -0,0 +1,35 @@
+import prisma from '@/lib/prisma';
+import type {
+  NextApiRequest,
+  NextApiResponse,
+} from 'next';
+import { withSentry } from '@sentry/nextjs';
+
+// Get total connections by profile owner
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { public_key } = req.query;
+  const connection = await prisma.connection.count({
+    where: {
+      profile_owner: public_key as string,
+    },
+    select: {
+      connector: true,
+    }
+  });
+
+  if (!connection) {
+    res.status(404).json({
+      error: 'Connection does not exist'
+    });
+    return;
+  }
+
+  res.status(200).json({
+    total_connections: connection.connector
+  });
+};
+
+export default withSentry(handler);
