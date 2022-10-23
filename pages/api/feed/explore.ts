7e4be30f7522eba34f24089e2de87dae9e68d308
Kunal Bagaria <kb24x7@gmail.com>
2022-10-23 23:44:31 +0530

    update: feed api query to latest articles first

--- pages/api/feed/explore.ts
+++ pages/api/feed/explore.ts
@@ -9,21 +9,15 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const from_date = new Date();
-  from_date.setDate(from_date.getDate() - 14);
-
   const articles = await prisma.article.findMany({
-    take: 100,
-    where: {
-      created_at: {
-        gte: from_date
-      }
+    take: 30,
+    orderBy: {
+      created_at: 'desc'
     },
     include: {
       owner: true
     }
   });
-  articles.sort((a, b) => b.views - a.views);
   res.status(200).json(articles);
 };
 
