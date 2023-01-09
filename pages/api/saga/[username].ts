3db90c3e6cb589a77b771defb6a9bc2b46530885
Kunal Bagaria <kb24x7@gmail.com>
2023-01-09 15:46:02 +0530

    add: Saga APIs

---
+++ pages/api/saga/[username].ts
@@ -0,0 +1,66 @@
+import prisma from '@/lib/prisma';
+import type {
+  NextApiRequest,
+  NextApiResponse,
+} from 'next';
+import { withSentry } from '@sentry/nextjs';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { username } = req.query;
+  const user = await prisma.user.findFirst({ where: {
+    username: username as string
+  }});
+  if (!user) {
+    return res.status(404).json({
+      error: 'User not found'
+    });
+  };
+  const connection_count = await prisma.connection.count({
+    where: { profile_owner: user.public_key }
+  });
+  const invited_by_inv = await prisma.invite.findFirst({
+    where: {
+      receiver: user.public_key
+    }
+  });
+  const post_count = await prisma.article.count({
+    where: {
+      owner: { id: user.id }
+    }
+  });
+  let invited_by;
+  if (invited_by_inv) {
+    invited_by = await prisma.user.findFirst({
+      where: {
+        id: invited_by_inv?.user_id
+      }
+    });
+  }
+  const userData = {
+    ...user,
+    connection_count,
+    invited_by: invited_by ? {
+      name: invited_by.name,
+      username: invited_by.username,
+    } : null
+  };
+  const articles = await prisma.article.findMany({
+    take: 10,
+    where: {
+      owner: { id: user.id }
+    },
+    orderBy: {
+      created_at: 'desc',
+    }
+  });
+  res.status(200).json({
+    user: userData,
+    articles: JSON.parse(JSON.stringify(articles)),
+    post_count
+  });
+};
+
+export default withSentry(handler);
---
+++ pages/api/saga/[username]/[slug].ts
@@ -0,0 +1,72 @@
+import prisma from '@/lib/prisma';
+import type {
+  NextApiRequest,
+  NextApiResponse,
+} from 'next';
+import { withSentry } from '@sentry/nextjs';
+import { getBlocks } from '@/lib/getArticleBlocks';
+
+async function handler (
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { username, slug } = req.query;
+  const article = await prisma.article.findFirst({
+    where: {
+      slug: slug as string,
+      owner: {
+        username: {
+          equals: username as string,
+          mode: 'insensitive'
+        }
+      }
+    }
+  });
+
+  if (!article) {
+    res.status(404).json({
+      error: 'Article not found'
+    });
+    return;
+  }
+
+  const user = await prisma.user.findFirst({
+    where: {
+      id: article.user_id
+    }
+  });
+  if (!user) {
+    res.status(404).json({
+      error: 'User not found'
+    });
+    return;
+  }
+
+  const arweave_data = await getBlocks(article.arweave_url, true);
+  if (!arweave_data) {
+    res.status(500).json({
+      error: 'Server error: Arweave data not found'
+    });
+    return;
+  }
+
+  const blocks = JSON.stringify(arweave_data.content.blocks);
+  if (!blocks) {
+    res.status(500).json({
+      error: 'Server error: Blocks not found'
+    });
+    return;
+  }
+
+  res.status(200).json({
+    article: JSON.parse(JSON.stringify(article)),
+    user_public_key: user.public_key,
+    username: user.username,
+    blocks,
+    user,
+    contentDigest: arweave_data?.contentDigest || '',
+  });
+
+}
+
+export default withSentry(handler);
